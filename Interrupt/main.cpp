48d05e3d21dd01ba3aa45736dea09e58c19b8b0d
Gruppe-11 <146166352+andsyv1@users.noreply.github.com>
2023-11-22 15:55:36 +0100

    Update main.cpp
    Den endelige kode som er blitt brukt i forsøket, gjorde noe endringer for å få til interrupten

--- Interrupt/main.cpp
+++ Interrupt/main.cpp
@@ -7,28 +7,46 @@
 
 
 #include <avr/io.h>
-#include <util/delay.h>
+#include <avr/interrupt.h>
 
-#define buttonPin 0   // Knappens inngangspinne (PD2)
-#define ledPin 6      // LED-utgangspinne (PD6)
+#define buttonPin PD0   // Knappens inngangspinne (PD2)
+#define ledPin PD6      // LED-utgangspinne (PD6)
 
-void setup() {
-	DDRD &= ~(1 << buttonPin); // Sett knappen som inngang (clear bit)
-	DDRD |= (1 << ledPin);     // Sett LED-en som utgang (set bit)
-	PORTD |= (1 << buttonPin); //Port D
+volatile uint8_t ledState = 0;  // 0 = av (knapp er ikke trykket)
+
+// Pin Change ISR-funksjon
+ISR(PCINT2_vect) {
+
+	// Sjekker om knappen er trykket
+	if (!(PIND & (1 << buttonPin))) {
+		// Bytt tilstanden til lysdioden
+		ledState = !ledState;
+		}
+		PORTD &= ~(1 << ledPin);
+	
 }
 
-int main() {
-	setup();
+int main(void) {
+	DDRD &= ~(1 << buttonPin);  // Sett knappen (PD2) som inngang
+	PORTD |= (1 << buttonPin);  // Aktiver pull-up motstand for knappen
+
+	DDRD |= (1 << ledPin);  // Sett lysdioden (PD6) som utgang
+
+	//pin change interrupts for PCINT18
+	PCICR |= (1 << PCIE2);
+
+	// Aktiver interrupts for den spesifikke pinnen (PD2)
+	PCMSK2 |= (1 << PCINT16);
+
+	// Aktiver globale interrupts
+	sei();
 
 	while (1) {
-		if (PIND & (1 << buttonPin)) {
-			PORTD |= (1 << ledPin); // Slå på LED hvis knappen er trykket
+		if (ledState) {
 			} else {
-			PORTD &= ~(1 << ledPin); // Slå av LED hvis knappen ikke er trykket
-		}
+			PORTD &= ~(1 << ledPin);
 	}
+		}
 
 	return 0;
 }
-
